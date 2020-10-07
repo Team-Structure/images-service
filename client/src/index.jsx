f57131f3456e87cb96634673e6ee9abf739ea351
George Humphrey <george.p.humphrey@gmail.com>
2020-10-06 22:07:05 -0400

    Complete basic client function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ImagesSelect from './modules/ImagesSelect.jsx';
-import ImageViewer from './modules/ImageViewer.jsx';
+import ImagesSelect from './modules/ImagesSelect';
+import ImageViewer from './modules/ImageViewer';
 
 class ProductImagesService extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productImages: ['https://teamstructureshopping.s3.amazonaws.com/id1/connect4_1.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_2.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_3.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_4.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_1.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_2.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_3.png', 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_4.png'],
+      productImages: [
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_1.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_2.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_3.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_4.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_1.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_2.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_3.png',
+        'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_4.png',
+      ],
       currentImage: 'https://teamstructureshopping.s3.amazonaws.com/id1/connect4_1.png',
     };
+
+    this.changeViewer = this.changeViewer.bind(this);
   }
 
   changeViewer(image) {
-    this.setState({ currentImage: image.target.src })
+    this.setState({ currentImage: image.target.src });
   }
-
-  componenentDidMount() {
-  }
-
-
 
   render() {
     return (
-      <div id='productImages'>
-        <ImagesSelect thumbnails={this.state.productImages} changeViewer={this.changeViewer.bind(this)} />
+      <div id="productImages">
+        <ImagesSelect thumbnails={this.state.productImages} changeViewer={this.changeViewer} />
         <ImageViewer image={this.state.currentImage} />
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   resolve: {
     modules: [
       path.join(__dirname, "src"), "node_modules"
-    ]
+    ],
+    extensions: ['.js', '.jsx']
   }
 };
